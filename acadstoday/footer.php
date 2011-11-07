79ba5ff81f837ea0234adc87585496e60289de65
Ashish Tajane <ashishtajane@hotmail.com>
2011-11-07 08:53:28 +0530

    minor changes in the footer.php

--- acadstoday/footer.php
+++ acadstoday/footer.php
@@ -1,16 +1,12 @@
 <div id="footer">
-	<div id="copyright">
+	<div id="copyright" style="float:left;">
 		<!-- automatically inserts the current year after copyright symbol -->
 		<p> &#169; <?php echo date(Y); ?>, AcadsToday </p>
 	</div>
-	<div>
-		<ul>
-			<!-- insert the appropriate links here -->
-			<!-- we may have to reorder the below list or add/remove links-->
-			<!-- CSS required to arrange these links horizontally and beautifully -->
-			<li><a href="">About Us</a></li>
-			<li><a href="">Contact Us</a></li>
-			<li><a href="">Privacy Policy</a></li>
-		</ul>
+	<div id="footerLinks" style="float:right;">
+		<!-- insert the appropriate links here -->
+		<!-- we may have to reorder the below list or add/remove links-->
+		<!-- CSS required to arrange these links horizontally and beautifully -->
+		<p><a href="">About Us</a> | <a href="">Contact Us</a> | <a href="">Privacy Policy</a></p>
 	</div>
 </div>
