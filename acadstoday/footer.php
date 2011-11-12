947da58f342005ddcc8861618393f1ea29789e22
Ashish Tajane <ashishtajane@hotmail.com>
2011-11-13 03:09:54 +0530

    added code to home.php, and a folder Misc. Files

--- acadstoday/footer.php
+++ acadstoday/footer.php
@@ -1,4 +1,4 @@
-<div id="footer">
+<div class ="footer" id="footer">
 	<div id="copyright" style="float:left;">
 		<!-- automatically inserts the current year after copyright symbol -->
 		<p> &#169; <?php echo date(Y); ?>, AcadsToday </p>
@@ -6,7 +6,11 @@
 	<div id="footerLinks" style="float:right;">
 		<!-- insert the appropriate links here -->
 		<!-- we may have to reorder the below list or add/remove links-->
-		<!-- CSS required to arrange these links horizontally and beautifully -->
-		<p><a href="">About Us</a> | <a href="">Contact Us</a> | <a href="">Privacy Policy</a></p>
+		<!-- <p><a href="">About Us</a> | <a href="">Contact Us</a> | <a href="">Privacy Policy</a></p> -->
+		<ul>
+			<li class="first"><a href="">About Us</a></li>
+			<li><a href="">Contact Us</a></li>
+			<li><a href="">Privacy Policy</a></li>
+		</ul>
 	</div>
 </div>
--- acadstoday/home.php
+++ acadstoday/home.php
@@ -7,6 +7,14 @@
 	<head>
 		<title>AcadsToday Home</title>
 		<?php include("header-head.php"); ?>
+		<link rel="stylesheet" type="text/css" href="css/home.css" />
+		
+		
+		<script type="text/javascript" src="js/tabber.js"></script>
+		<!-- <script type="text/javascript" src="js/tabber-minimized.js"></script> -->
+		<link rel="stylesheet" type="text/css" href="css/tabber.css" />
+		
+		
 	</head>
 	<body>
 		<div class="wrapper">
@@ -27,7 +35,7 @@
 						$result = mysql_query("SELECT * FROM user where user_id = " . $user_id) or die(mysql_error());  
 						$row = mysql_fetch_array( $result );
 						echo "Name: ".$row['user_name']."<br />";
-						echo "Gender: ".$row['gender']; //show 'about me' here
+						echo "Gender: ".$row['gender']; //show 'about me' here or other useful links
 					?>
 				</div>
 			</div>
@@ -35,12 +43,38 @@
 			
 			<!-- central area starts which is showing name and details of user and all notifications like FB wall -->
 			<div id="center">
-				
+				<div class="tabber">
+					<div class="tabbertab" title="Courses">
+						<p>Tab 1 content.</p>
+					</div>
+					<div class="tabbertab" title="Instructors">
+						<p>Tab 2 content.</p>
+					</div>
+					<div class="tabbertab" title="Users">
+						<p>Tab 3 content.</p>
+					</div>
+				</div>
 			</div>
 			<!-- central area ends-->
 			
 			<!-- right panel starts which has search box and togglable list of courses taken/instructors followed etc. --> 
 			<div id="rightpanel">
+				<form class="searchform">
+					<input class="searchfield" type="text" value="Search Here" onfocus="if (this.value == 'Search Here') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Search Here';}" />
+					<input class="searchbutton" type="button" value="Go" />
+				</form>
+				
+				<div class="tabber">
+					<div class="tabbertab" title="Courses">
+						<p>Tab 1 content.</p>
+					</div>
+					<div class="tabbertab" title="Instructors">
+						<p>Tab 2 content.</p>
+					</div>
+					<div class="tabbertab" title="Users">
+						<p>Tab 3 content.</p>
+					</div>
+				</div>
 			</div>
 			<!-- right panel ends-->
 			<div class="push"></div>
